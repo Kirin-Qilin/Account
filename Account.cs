d124d91131443e0458a92beca322e4cd956a4b99
Kirin-Qilin <madigan.haselwood@gmail.com>
2026-02-02 13:44:12 -0600

    Add initial build artifacts and project configuration for Account Class
    - Created Account Class.dll and Account Class.refint.dll as part of the build process.
    - Added project.assets.json to manage dependencies and project settings for .NET 9.0.
    - Included project.nuget.cache to track NuGet package cache state and expected package files.
    - Configured assembly metadata and trust information in the generated DLLs.

---
+++ Account.cs
@@ -0,0 +1,75 @@
+using System.Dynamic;
+using System.Globalization;
+
+public class Account
+{
+    private float balance;
+    private string ownerAcc;
+
+    public float Balance
+    {
+        get { return balance; }
+        set{if (value >= 0){balance = value;} }
+    }
+    public string OwnerAcc
+    {
+        get{ return ownerAcc; }
+    }
+    
+
+    public Account(float balance, string ownerAcc)
+    {
+        this.balance = balance;
+        this.ownerAcc = ownerAcc;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine($"Balance: {balance}  Account Name:  {ownerAcc}");
+    }
+    public float Deposit(float amount)
+    {
+        balance += amount;
+        return balance;
+    }
+
+    public float GetBalance()
+    {
+        return balance;
+    }
+
+    public bool Withdraw(float amount)
+    {
+        if (balance - amount < 0)
+        {
+            //Console.WriteLine("insufficiant funds");
+            return false;
+        }
+
+        else
+        {
+            balance -= amount;
+            return true;
+        }
+
+    }
+
+    
+
+ 
+
+
+    public bool Transfer(Account accountDos, float amount)
+    {
+        if (balance - amount < 0)
+        {
+            return false;
+        }
+        else
+        {
+            balance = balance - amount;
+            accountDos.balance += amount;
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+public class Program
+{
+            public static void doIt(Account a, int total)
+    {
+        a.Withdraw(50);
+        total--;
+    }
+    
+        public static void Test(int? x)
+    {
+        if (x != null)
+        {
+            Console.WriteLine(x);
+        }
+        else
+            Console.WriteLine("x does not havce a value");
+    }
+    static void Main(string[] args)
+    {
+        /*Console.WriteLine("Testing account one");
+        Account myAccount = new Account(300, "Account1");
+        myAccount.Show();
+        Console.WriteLine("Going to withdraw 400 from account 1");
+        myAccount.Withdraw(400);
+        myAccount.Show();
+        myAccount.Withdraw(200);
+        myAccount.Show();
+        myAccount.Deposit(600);
+        myAccount.Show();
+        Console.WriteLine("Testing account 2");
+        Account account2 = new Account(500, "Account2");
+        myAccount.Transfer(account2, 10000);
+        myAccount.Show();
+        account2.Show();
+        myAccount.Transfer(account2, 100);
+        account2.Show();
+        myAccount.Show();*/
+
+        // Account testAccount = new Account(400, "ACC TEST");
+        // testAccount.Balance = 10;
+        // Console.WriteLine(testAccount.Balance);
+        // testAccount.Balance = -10;
+        // Console.WriteLine(testAccount.Balance);
+        // Console.WriteLine(testAccount.OwnerAcc);
+
+
+
+
+
+        /* Account a1 = new Account(100, "bob");
+         int totalAssets = 1000;
+         doIt(a1, totalAssets);
+         Console.WriteLine("after doIt:");
+         a1.Show();
+         Console.WriteLine($"total assets is now {totalAssets}");*/
+
+        /* int? x1 = 20;
+         int? x2 = null;
+         Test(x1);
+         Test(x2);8*/
+
+        Account? referenceVar = null;
+
+        
+
+    }
+
+
+}
+
